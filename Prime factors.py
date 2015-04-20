eae1988454af258eeac2e5f3db9e027cfa33358d
gorillainvestor <JimYin88@gmail.com>
2015-04-19 21:29:30 -0400

    Create Prime factors.py

---
+++ Prime factors.py
@@ -0,0 +1,15 @@
+def prime_factor(n):
+    result = []
+    remainder = n
+    factor = 2
+    while remainder > 1:
+        if remainder % factor == 0:
+            result.append(factor)
+            remainder /= factor
+        else:
+            if factor == 2:
+                factor = 3
+            else:
+                factor += 2
+            
+    return result
