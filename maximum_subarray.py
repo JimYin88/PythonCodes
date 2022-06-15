609de365174a9b9dbf40b73dc6c2a3bb69144ae3
Jim Yin <jimyin88@gmail.com>
2022-06-15 09:32:01 -0400

    Improved version

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,12 +1,19 @@
-def max_subarray(L):
+'''
+Created on Jun 15, 2022
+
+@author: JimYi
+'''
+
+
+def max_subarray(l):
     current_sum = 0
     current_index = -1
     best_sum = 0
     best_start_index = -1
     best_end_index = -1
     
-    for i in xrange(len(L)):
-        val = current_sum + L[i]
+    for i in range(len(l)):
+        val = current_sum + l[i]
         if val > 0:
             if current_sum == 0:
                 current_index = i
@@ -19,6 +26,6 @@ def max_subarray(L):
             best_start_index = current_index
             best_end_index = i
             
-    return L[best_start_index:best_end_index+1]
+    return l[best_start_index:best_end_index+1]
 
-print max_subarray([-3, 9, 2, 1, -15, 3, 2, -6, 8, 8])
+print(max_subarray([-3, 9, 2, 1, -15, 3, 2, -6, 8, 8]))
